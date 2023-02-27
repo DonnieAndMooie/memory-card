7fd7c4bb5b2ba1830a392260ae1844a0b8b79d11
DonnieAndMooie <owenjeaves@gmail.com>
2023-02-27 22:34:35 +0000

    Create files for all components

---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../Card'
+
+export default function Game() {
+  return (
+    <div>
+      <div>Game</div>
+      <Card></Card>
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Scoreboard from './Scoreboard'
+
+export default function Header() {
+  return (
+    <div>
+      <div>Header</div>
+      <Scoreboard></Scoreboard>
+    </div>
+    
+  )
+}
